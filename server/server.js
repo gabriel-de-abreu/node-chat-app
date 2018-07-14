fae169bf609c9bddcf0c2d48408bcdb7ae853b06
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-13 22:33:55 -0300

    add connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path = require('path');
 const express = require('express');
+const socketIO= require('socket.io');
+const http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const port = process.env.PORT || 3000; 
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+    socket.on('disconnect',()=>{
+        console.log("batata");
+    });
+});
+
+
+
+server.listen(port,()=>{
     console.log(`Server started at port ${port}`);
 });
