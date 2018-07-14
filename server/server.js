9d7f60af58382e9d04401e0d062c3144e4c16b6f
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-13 23:44:20 -0300

    emit message on create

--- server/server.js
+++ server/server.js
@@ -14,20 +14,17 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log('new user connected');
-
-    socket.emit('newMessage',{
-        from: "me",
-        text:"hey",
-        createAt: 321321
-    });
-    
     
     socket.on('disconnect',()=>{
         console.log("batata");
     });
 
     socket.on('createMessage',(message)=>{
-        console.log('Create Message',message);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        })
     });  
 });
 
