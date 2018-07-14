11608a9c8cbc540524e6c15aefe60f97ef14340d
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-14 18:03:22 -0300

    basic gui

--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,23 @@ socket.on('connect', () => {
 
 socket.on('newMessage',(message)=>{
     console.log('new message', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    $('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
 }); 
 
-socket.emit('createMessage',{
-    from: "fulano",
-    text: "hi"
-},function(data){
-    console.log(data);
-});
+$('#message-form').on('submit',(e)=>{
+    e.preventDefault();
 
+    socket.emit('createMessage',{
+        from: 'User',
+        text: $('[name=message]').val()
+    },()=>{
+
+    });
+    $("[name=message]").val('');
+});
